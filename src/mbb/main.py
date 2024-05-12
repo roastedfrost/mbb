8e88d938fc9af6d831fc8d0c588a9cab396380c1
roastedfrost <roastedfrost@gmail.com>
2024-05-12 23:51:34 +0300

    host/port server configuration

--- src/mbb/main.py
+++ src/mbb/main.py
@@ -10,11 +10,5 @@ application = FastAPI()
 application.include_router(moex_router, prefix="/moex")
 
 
-@application.get("/ping")
-def pong():
-    return "pong"
-
-
 def start():
-    print(settings.model_dump())
-    uvicorn.run("mbb.main:application", host="0.0.0.0", port=8008, reload=True)
+    uvicorn.run("mbb.main:application", host=settings.host, port=settings.port, reload=True)
--- src/mbb/settings.py
+++ src/mbb/settings.py
@@ -3,6 +3,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     db_name: str
+    host: str
+    port: int
     model_config = SettingsConfigDict(env_file='.env')
 
 
